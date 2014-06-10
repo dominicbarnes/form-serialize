4eacfa17f3a0ca0157baa4af209ac016dfd19c0d
Dominic Barnes <dominic@dbarnes.info>
2014-06-10 14:04:15 -0400

    dropping testling, using component-test + adding 'loose' param

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var reduce = require("reduce");
 var value = require("value");
 var submittable = require("submittable");
 
-module.exports = function (form) {
+module.exports = function (form, loose) {
     return reduce(form.elements, function (acc, el) {
-        if (submittable(el)) dot.set(acc, el.name, value(el));
+        if ((loose && el.name) || submittable(el)) {
+            dot.set(acc, el.name, value(el));
+        }
         return acc;
     }, {});
 };
